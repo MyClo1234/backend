5b4b942fad7bb6a54f1fc2c957c7d242bedaf530
Yoonsik-Shin <sys25800994@gmail.com>
2026-01-24 00:55:36 +0900

    Test: cicd

--- app/routers/health_routes.py
+++ app/routers/health_routes.py
@@ -6,4 +6,4 @@ health_router = APIRouter()
 
 @health_router.get("/health")
 def health():
-    return JSONResponse(content={"status": "yes, i'm ok"})
+    return JSONResponse(content={"status": "ok ok ok"})
